5e0c10c07aa644362aabf81bc915fcab4add881e
Camila <cami.martinez@ida.cl>
2018-12-27 11:20:01 -0300

    fin

--- app/assets/scripts/general.js
+++ app/assets/scripts/general.js
@@ -9,58 +9,97 @@ $(document).ready(function() {
       $('.tooltips').remove();
    })
 
+   ///GRUPOS DE ACCIONES PARA LOS PROYECTOS, PASAR DE ETAPA O ASIGNAR EVALUADORES
    var selector = $('#select_action');
-   selector.change(function () {
+   selector.change(function() {
       if ($(this).val() == 'evaluadores') {
          $('.group-evaluadores').show();
          $('.group-etapa').hide();
-      }else if ($(this).val() == 'etapa') {
+      } else if ($(this).val() == 'etapa') {
          $('.group-etapa').show();
          $('.group-evaluadores').hide();
-      }else {
+      } else {
          $('.group-etapa').hide();
          $('.group-evaluadores').hide();
       }
    });
 
-   $('[name="master"]').on('change', function(ev){
+   ///CHECK MASTER DE LA TABLE DE LOS PROYECTOS
+   $('[name="master"]').on('change', function(ev) {
       var valor = $(ev.currentTarget).prop("checked");
-      if(!valor){
+      if (!valor) {
          $('[type="checkbox"]').prop("checked", false);
       }
-      if(valor){
+      if (valor) {
          $('[type="checkbox"]').prop("checked", true);
       }
    });
 
-   $( "#btn-login" ).click(function() {
-     $( ".module__login" ).toggle();
-     console.log('TEST1');
+   ///BOTÓN LOGIN
+   $("#btn-login").click(function() {
+      $(".module__login").toggle();
+      console.log('TEST1');
    });
+
+
+   ///REPEATER FORM DOCENTE Y ESTUDIANTE
+   $('.repeater-docente ').repeater({
+      initEmpty: false,
+      show: function() {
+         var selfRepeaterItem = this;
+         $(selfRepeaterItem).slideDown();
+
+         var repeaterItems = $(".repeatable-docente > [data-repeater-item]");
+         console.log(repeaterItems);
+         $(selfRepeaterItem).attr('data-index', repeaterItems.length - 1);
+         $(selfRepeaterItem).find('[data-repeater-count]').text(repeaterItems.length);
+      },
+      hide: function() {
+         $(this).slideUp();
+      }
+   });
+
+
+   $('.repeater-estudiante ').repeater({
+      initEmpty: false,
+      show: function() {
+         var selfRepeaterItem = this;
+         $(selfRepeaterItem).slideDown();
+
+         var repeaterItems = $(".repeatable-estudiante > [data-repeater-item]");
+         console.log(repeaterItems);
+         $(selfRepeaterItem).attr('data-index', repeaterItems.length - 1);
+         $(selfRepeaterItem).find('[data-repeater-count]').text(repeaterItems.length);
+      },
+      hide: function() {
+         $(this).slideUp();
+      }
+   });
+
 
 });
 
-$(document).ready(function (e) {
+$(document).ready(function(e) {
    function t(t) {
-       e(t).bind("click", function (t) {
-           t.preventDefault();
-           e(this).parent().fadeOut()
-       })
+      e(t).bind("click", function(t) {
+         t.preventDefault();
+         e(this).parent().fadeOut()
+      })
    }
-   e(".dropdown-toggle").click(function () {
-       var t = e(this).parents(".button__simple--select").children(".dropdown-menu").is(":hidden");
-       e(".button__simple--select .dropdown-menu").hide();
-       e(".button__simple--select .dropdown-toggle").removeClass("active");
-       if (t) {
-           e(this).parents(".button__simple--select").children(".dropdown-menu").toggle().parents(".button__simple--select").children(".dropdown-toggle").addClass("active")
-       }
+   e(".dropdown-toggle").click(function() {
+      var t = e(this).parents(".button__simple--select").children(".dropdown-menu").is(":hidden");
+      e(".button__simple--select .dropdown-menu").hide();
+      e(".button__simple--select .dropdown-toggle").removeClass("active");
+      if (t) {
+         e(this).parents(".button__simple--select").children(".dropdown-menu").toggle().parents(".button__simple--select").children(".dropdown-toggle").addClass("active")
+      }
    });
-   e(document).bind("click", function (t) {
-       var n = e(t.target);
-       if (!n.parents().hasClass("button__simple--select")) e(".button__simple--select .dropdown-menu").hide();
+   e(document).bind("click", function(t) {
+      var n = e(t.target);
+      if (!n.parents().hasClass("button__simple--select")) e(".button__simple--select .dropdown-menu").hide();
    });
-   e(document).bind("click", function (t) {
-       var n = e(t.target);
-       if (!n.parents().hasClass("button__simple--select")) e(".button__simple--select .dropdown-toggle").removeClass("active");
+   e(document).bind("click", function(t) {
+      var n = e(t.target);
+      if (!n.parents().hasClass("button__simple--select")) e(".button__simple--select .dropdown-toggle").removeClass("active");
    })
 });
